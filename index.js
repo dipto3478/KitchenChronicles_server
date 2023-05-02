330a519f2b970641e59b169a43648f547e422c2d
dipto3478 <kitkatboy3478@gmail.com>
2023-05-02 10:53:03 +0600

    now data running..

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-// const chefData = require("./data/chefData.json");
+const chefData = require("./data/chefData.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("server running..........");
 });
-// app.get("/chefs", (req, res) => {
-//   res.send(chefData);
-// });
+app.get("/chefs", (req, res) => {
+  res.send(chefData);
+});
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
