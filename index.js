b3e6f7a5c9a2e5b304ad3a0df16a1399b2814a73
dipto3478 <kitkatboy3478@gmail.com>
2023-05-02 10:52:01 +0600

    cors add

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 // const chefData = require("./data/chefData.json");
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("server running..........");
 });
