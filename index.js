5d1f7b9448e671d800de957c8d669a4923f97214
dipto3478 <kitkatboy3478@gmail.com>
2023-05-02 14:05:47 +0600

    fixed some issues

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get("/", (req, res) => {
 app.get("/chefs", (req, res) => {
   res.send(chefData);
 });
+app.get("/chefs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const chef = chefData.find((cd) => cd.chef.id === id);
+  res.send(chef);
+});
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
