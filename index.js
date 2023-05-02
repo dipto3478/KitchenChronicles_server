72367bf9bc934cb5d80e6cbf450f26e1abbc08f0
dipto3478 <kitkatboy3478@gmail.com>
2023-05-02 10:48:55 +0600

    now data dot json file add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-const chefData = require("./data/chefData.json");
+// const chefData = require("./data/chefData.json");
 
 app.get("/", (req, res) => {
   res.send("server running..........");
